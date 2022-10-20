12fc8157212f44d1a4c64c1db786935546fab3c5
Thiruvengadam A.I <109656843+Thiruvengadam2002@users.noreply.github.com>
2022-10-20 16:35:45 +0530

    Add files via upload

---
+++ pro5.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title> second </title>
+</head>
+<body>
+<?php
+$message_1 = "Good Morning.";
+$message_2 = "Have a nice day!";
+echo $message_1." ". $message_2;
+?>
+</body>
+</html>
